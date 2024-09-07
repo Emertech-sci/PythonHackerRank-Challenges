d04100a5a6fd4dd388c2bbfe19f4a3294cbcefde
Mateo Rendon MacBook <rendoncode@gmail.com>
2024-09-06 23:02:38 -0600

    Challenges primera estrella

---
+++ pythonProject/arithmeticOperators.py
@@ -0,0 +1,14 @@
+print("Introduce dos numeros a operar")
+a = int(input())
+b = int(input())
+suma = a + b
+resta = a - b
+producto = a * b
+int_division = a // b
+double_division = a / b
+
+print(f"el resultado de {a} + {b} = {suma}")
+print(f"el resultado de {a} - {b} = {resta}")
+print(f"el resultado de {a} X {b} = {producto}")
+print(f"el resultado de {a} // {b} = {int_division}")
+print(f"el resultado de {a} / {b} = {double_division}")
